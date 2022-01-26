c180097a4437456629b2c174427a74c88a497888
to-sho-volodya <keklolorbedol13@gmail.com>
2022-01-26 18:01:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    /*arrows:true,*/
+    /*dots: true,
+    infinite: true,
+    speed: 500,*/
+    fade: true,
+    autoplay:true,
+    autoplaySpeed: 4000,
+   /* cssEase: 'linear'*/
+   /* arrows:true,
+    dots:true,
+    adaptiveHeight:true,
+    infinite:false,
+    draggable:false,*/
+  });
+});
