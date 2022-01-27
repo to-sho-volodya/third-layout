9508b9ea5525e534393dbf969e9eb8531ee4fed9
to-sho-volodya <keklolorbedol13@gmail.com>
2022-01-27 18:02:13 +0200

    validation

--- js/script.js
+++ js/script.js
@@ -1,40 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.slider').slick({
-    /*arrows:true,*/
+
     /*dots: true,
     infinite: true,
     speed: 500,*/
     fade: true,
-    autoplay:true,
+    autoplay: true,
     autoplaySpeed: 4000,
-   /* cssEase: 'linear'*/
-   /* arrows:true,
-    dots:true,
-    adaptiveHeight:true,
-    infinite:false,
-    draggable:false,*/
+    infinite: true,
+    arrows: true,
+    /* cssEase: 'linear'*/
+    /* arrows:true,
+     dots:true,
+     adaptiveHeight:true,
+
+     draggable:false,*/
   });
 });
 
-$('.feedback-form').on('submit', e => {
-  e.preventDefault()
-  e.stopPropagation()
-  console.log('submit')
-})
+/*$('.feedback-form').on('submit', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log('submit');
+});*/
 
 
-/*
-var form = document.querySelector('.feedback-form')
-var validateBtn = form.querySelector('.validateBtn')
-var name = form.querySelector('name')
-var phone = form.querySelector('phone')
-var email = form.querySelector('email')
-var field = form.querySelectorAll('.field')
-
+var form = document.querySelector('.feedback-form');
+var validateBtn = form.querySelector('.validateBtn');
+var name = form.querySelector('name');
+var phone = form.querySelector('phone');
+var email = form.querySelector('email');
+var field = form.querySelectorAll('.field');
 
 
 form.addEventListener('submit', function (event) {
-  event.preventDefault()
-  event.stopPropagation()
-  console.log('clicked on validate')
-})*/
+  event.preventDefault();
+  event.stopPropagation();
+
+  var errors = form.querySelectorAll('.error');
+
+  for (var i = 0; i < errors.length; i++) {
+    errors[i].remove();
+  }
+
+  for (var i = 0; i < field.length; i++) {
+    if (!field[i].value) {
+      console.log('field is blank', field[i]);
+      var error = document.createElement('div');
+      error.className = 'error';
+      error.style.color = 'red';
+      error.innerText = 'Поле обязательно для заполнения';
+      form[i].parentElement.insertBefore(error, field[i]);
+    }
+  }
+});
+
+
