9325e8a7d73c9fdb7d3eb77a3a059da4a763520d
to-sho-volodya <keklolorbedol13@gmail.com>
2022-01-27 14:44:38 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ $(document).ready(function(){
     draggable:false,*/
   });
 });
+
+$('.feedback-form').on('submit', e => {
+  e.preventDefault()
+  e.stopPropagation()
+  console.log('submit')
+})
+
+
+/*
+var form = document.querySelector('.feedback-form')
+var validateBtn = form.querySelector('.validateBtn')
+var name = form.querySelector('name')
+var phone = form.querySelector('phone')
+var email = form.querySelector('email')
+var field = form.querySelectorAll('.field')
+
+
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+  console.log('clicked on validate')
+})*/
